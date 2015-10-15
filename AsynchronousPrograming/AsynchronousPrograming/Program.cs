e2f326859d933401ab94ec1aeafc80d70078d7a1
bstaykov <boycho.staykov@gmail.com>
2015-10-15 10:23:41 +0300

    stylecop

---
+++ AsynchronousPrograming/AsynchronousPrograming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousPrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 20,
+                index => 
+                {
+                    if (index % 2 == 0)
+                    {
+                        Console.WriteLine(index);
+                    }
+                });
+        }
+    }
+}
